afbcc63c0fdcac7ee428bc45072f3625d9520216
thanhantran1 <thanhantran21@gmail.com>
2021-10-08 15:08:34 +0700

    add function check user is logged in

--- server/routers/auth.routers.js
+++ server/routers/auth.routers.js
@@ -8,8 +8,16 @@ const User = require('../models/User.models')
 // @route GET api/auth
 // @desc check if user is logged in
 // @access Public
-router.get('/', (req, res) => {
-    res.send('user router')
+router.get('/', async (req, res) => {
+    try {
+        const user = await User.findById(req.userId).select('-password')
+        if (!user)
+            return res.status(400).json({ success: false, message: 'User not found' })
+        res.json({ success: true, user })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ success: false, message: 'Internal server error' })
+    }
 })
 
 // @route POST api/auth/resgister
